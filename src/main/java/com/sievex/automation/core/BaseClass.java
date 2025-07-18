d2ac92c4b9fee6b2aef22e3274b7ef5deb98fe23
yashtank09 <yashtank09@gmail.com>
2025-07-19 03:24:11 +0530

    Add PropertyLoader import, update Crawler interface, and create automation properties file

--- src/main/java/com/sievex/automation/core/BaseClass.java
+++ src/main/java/com/sievex/automation/core/BaseClass.java
@@ -1,5 +1,6 @@
 package com.sievex.automation.core;
 
+import com.sievex.automation.util.PropertyLoader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
--- src/main/java/com/sievex/automation/crawling/Crawler.java
+++ src/main/java/com/sievex/automation/crawling/Crawler.java
@@ -1,5 +1,6 @@
 package com.sievex.automation.crawling;
 
 public interface Crawler {
-
+    public void crawl(String job);
+    public boolean supports(String domain);
 }
